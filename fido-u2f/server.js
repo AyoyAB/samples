58801c5bf82bbab4e1c855b43b8a5703c2db4ee0
Oscar Jacobsson <oscar@ayoy.se>
2014-12-08 21:57:57 +0100

    Adding skeleton Bootstrap web app structure

---
+++ fido-u2f/server.js
@@ -0,0 +1,23 @@
+var express = require('express'),
+    path    = require('path'),
+    app     = express();
+
+// Set the TCP port to list to.
+app.set('port',        process.env.PORT || 3000);
+
+// Set up Jade as the view engine.
+app.set('views',       path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// Use the public folder for static files.
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set up manual routes.
+app.get('/', function(req, res) {
+    res.render('index', { title: 'FIDO U2F Demo' });
+});
+
+// Start the server.
+app.listen(app.get('port'), function () {
+    console.log('Server listening on port: %s.', app.get('port'));
+});
