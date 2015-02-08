1cce42c79e382a33168af4b9b6f7356652bda371
Oscar Jacobsson <oscar@ayoy.se>
2015-02-08 11:48:19 +0100

    Documented the main application controller

--- fido-u2f/public/js/controllers/fido-controller.js
+++ fido-u2f/public/js/controllers/fido-controller.js
@@ -1,30 +1,77 @@
 (function() {
     'use strict';
 
+    /**
+     * The main demo application controller.
+     */
     angular
         .module('fidoApp')
         .controller('FidoController', [
             'CryptoService', 'FidoService', 'StorageService', function(CryptoService, FidoService, StorageService) {
+                /**
+                 * Generates the FIDO U2F random registration challenge.
+                 *
+                 * @returns {ng.IPromise} The generated registration challenge.
+                 *
+                 * @private
+                 */
+                function _generateRegistrationChallenge() {
+                    return CryptoService.generateChallenge(32);
+                }
+
                 var vm = this;
 
+                /**
+                 * Checks if the Web Crypto API is supported by the browser.
+                 *
+                 * @returns {boolean} True if Web Crypto API is supported.
+                 */
                 vm.isWebCryptoSupported = function() {
                     return CryptoService.isSupported();
                 };
 
+                /**
+                 * Checks if the Web Storage API is supported by the browser.
+                 *
+                 * @returns {boolean} True if Web Storage API is supported.
+                 */
                 vm.isWebStorageSupported = function() {
                     return StorageService.isSupported();
                 };
 
-                vm.isFidoU2FAddOnInstalled = function() {
+                /**
+                 * Checks if the FIDO U2F API is supported by the browser.
+                 *
+                 * @returns {boolean} True if FIDO U2F API is supported.
+                 */
+                vm.isFidoU2FSupported = function() {
                     return FidoService.isSupported();
                 };
 
-                vm.generateChallenge = function() {
-                    return CryptoService.generateChallenge(32);
+                /**
+                 * Checks if all pre-requisites are met.
+                 *
+                 * @returns {boolean} True if all pre-requisites are met.
+                 */
+                vm.isAllSet = function() {
+                   return vm.isWebCryptoSupported() && vm.isWebStorageSupported() && vm.isFidoU2FSupported();
                 };
 
-                vm.register = function(challenge, appId) {
-                    return FidoService.register(challenge, appId);
+                /**
+                 * Performs FIDO U2F registration.
+                 *
+                 * @param {string} appId The application identifier.
+                 */
+                vm.register = function(appId) {
+                    _generateRegistrationChallenge()
+                        .then(function (challenge) {
+                            FidoService.register(challenge, appId)
+                                .then(function (result) {
+                                    // TODO: Display and store result.
+                                }, function (error) {
+                                    // TODO: Display error.
+                                });
+                    });
                 };
         }]);
 })();
