0a35adfebdcd0b54b1ff6decbd190458d67df444
Oscar Jacobsson <oscar@ayoy.se>
2015-02-08 18:42:51 +0100

    Added registration, but it's not working yet...

--- fido-u2f/public/js/controllers/fido-controller.js
+++ fido-u2f/public/js/controllers/fido-controller.js
@@ -9,6 +9,20 @@
         .controller('FidoController', [
             'CryptoService', 'FidoService', 'StorageService', function(CryptoService, FidoService, StorageService) {
                 /**
+                 * Controller startup function.
+                 *
+                 * @private
+                 */
+                function _activate() {
+                    vm.registerRequest.version  = FidoService.getFidoVersion();
+                    vm.registerRequest.appId    = vm.APPLICATION_ID;
+
+                    _generateRegistrationChallenge().then(function (challenge) {
+                        vm.registerRequest.challenge = challenge;
+                    });
+                }
+
+                /**
                  * Generates the FIDO U2F random registration challenge.
                  *
                  * @returns {ng.IPromise} The generated registration challenge.
@@ -19,59 +33,72 @@
                     return CryptoService.generateChallenge(32);
                 }
 
-                var vm = this;
-
                 /**
                  * Checks if the Web Crypto API is supported by the browser.
                  *
                  * @returns {boolean} True if Web Crypto API is supported.
                  */
-                vm.isWebCryptoSupported = function() {
+                function isWebCryptoSupported() {
                     return CryptoService.isSupported();
-                };
+                }
 
                 /**
                  * Checks if the Web Storage API is supported by the browser.
                  *
                  * @returns {boolean} True if Web Storage API is supported.
                  */
-                vm.isWebStorageSupported = function() {
+                function isWebStorageSupported() {
                     return StorageService.isSupported();
-                };
+                }
 
                 /**
                  * Checks if the FIDO U2F API is supported by the browser.
                  *
                  * @returns {boolean} True if FIDO U2F API is supported.
                  */
-                vm.isFidoU2FSupported = function() {
+                function isFidoU2FSupported() {
                     return FidoService.isSupported();
-                };
+                }
 
                 /**
                  * Checks if all pre-requisites are met.
                  *
                  * @returns {boolean} True if all pre-requisites are met.
                  */
-                vm.isAllSet = function() {
-                   return vm.isWebCryptoSupported() && vm.isWebStorageSupported() && vm.isFidoU2FSupported();
-                };
+                function isAllSet() {
+                    return vm.isWebCryptoSupported() && vm.isWebStorageSupported() && vm.isFidoU2FSupported();
+                }
 
                 /**
                  * Performs FIDO U2F registration.
-                 *
-                 * @param {string} appId The application identifier.
                  */
-                vm.register = function(appId) {
-                    _generateRegistrationChallenge()
-                        .then(function (challenge) {
-                            FidoService.register(challenge, appId)
-                                .then(function (result) {
-                                    // TODO: Display and store result.
-                                }, function (error) {
-                                    // TODO: Display error.
-                                });
-                    });
-                };
+                function register() {
+                    FidoService.register(vm.registerRequest.challenge, vm.registerRequest.appId)
+                        .then(function (result) {
+                            // Store the result.
+                            vm.registrationResponse = result;
+
+                            // TODO: We'll need to work on our state management.
+                            StorageService.setItem('registration-result', result);
+
+                        }, function (error) {
+                            // Store the error.
+                            vm.registrationError = error;
+                        });
+                }
+
+                var vm = this;
+
+                vm.isWebCryptoSupported     = isWebCryptoSupported;
+                vm.isWebStorageSupported    = isWebStorageSupported;
+                vm.isFidoU2FSupported       = isFidoU2FSupported;
+                vm.isAllSet                 = isAllSet;
+                vm.register                 = register;
+                vm.APPLICATION_ID           = 'http://localhost:3000/';
+                vm.registerRequest          = {};
+                vm.registrationError        = null;
+                vm.registrationResponse     = null;
+
+                _activate();
         }]);
 })();
