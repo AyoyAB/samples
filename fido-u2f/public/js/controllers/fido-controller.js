aca94baf579d3fa74238c392f383df9ead061efd
Oscar Jacobsson <oscar@ayoy.se>
2015-02-08 08:21:08 +0100

    Added web local storage service

--- fido-u2f/public/js/controllers/fido-controller.js
+++ fido-u2f/public/js/controllers/fido-controller.js
@@ -3,23 +3,28 @@
 
     angular
         .module('fidoApp')
-        .controller('FidoController', ['CryptoService', 'FidoService', function(CryptoService, FidoService) {
-            var vm = this;
+        .controller('FidoController', [
+            'CryptoService', 'FidoService', 'StorageService', function(CryptoService, FidoService, StorageService) {
+                var vm = this;
 
-            vm.isWebCryptoSupported = function() {
-                return CryptoService.isWebCryptoSupported();
-            };
+                vm.isWebCryptoSupported = function() {
+                    return CryptoService.isWebCryptoSupported();
+                };
 
-            vm.isFidoU2FAddOnInstalled = function() {
-                return FidoService.isFidoU2FAddOnInstalled();
-            };
+                vm.isWebStorageSupported = function() {
+                    return StorageService.isSupported();
+                };
 
-            vm.generateChallenge = function() {
-                return CryptoService.generateChallenge();
-            };
+                vm.isFidoU2FAddOnInstalled = function() {
+                    return FidoService.isFidoU2FAddOnInstalled();
+                };
 
-            vm.register = function(challenge, appId) {
-                return FidoService.register(challenge, appId);
-            };
+                vm.generateChallenge = function() {
+                    return CryptoService.generateChallenge();
+                };
+
+                vm.register = function(challenge, appId) {
+                    return FidoService.register(challenge, appId);
+                };
         }]);
 })();
---
+++ fido-u2f/public/js/services/storage-service.js
@@ -0,0 +1,60 @@
+(function() {
+    'use strict';
+
+    /**
+     * Web Storage API Service.
+     */
+    angular
+        .module('fidoApp')
+        .factory('StorageService', ['$window', function($window) {
+            /**
+             * Checks if Web Local Storage is supported in the current browser.
+             *
+             * @returns {boolean} True if Web Storage is supported.
+             */
+            function isSupported() {
+                return $window['localStorage'] !== undefined;
+            }
+
+            /**
+             * Checks if a given key is present in Web Local Storage.
+             *
+             * @param key           The key to check.
+             *
+             * @returns {boolean}   True if the key is present.
+             */
+            function hasItem(key) {
+                return $window['localStorage'].getItem(key) !== null;
+            }
+
+            /**
+             * Gets the value for the specified key from Web Local Storage.
+             *
+             * @param key   The key of the value to fetch.
+             *
+             * @returns {*} The requested value, or null if none was found.
+             */
+            function getItem(key) {
+                var value = $window['localStorage'].getItem(key);
+
+                return (value === null) ? null : angular.fromJson(value);
+            }
+
+            /**
+             * Stores the value for the specified key in Web Local Storage.
+             *
+             * @param key   The key to store the value under.
+             * @param value The value to store.
+             */
+            function setItem(key, value) {
+                $window['localStorage'].setItem(key, angular.toJson(value));
+            }
+
+            return {
+                isSupported:    isSupported,
+                hasItem:        hasItem,
+                getItem:        getItem,
+                setItem:        setItem
+            };
+        }]);
+})();
